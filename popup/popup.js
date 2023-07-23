fe4afad9fee2b4617701d22e4cc1e1b0a6d0244a
Engin Karataş <enginkaratas99@gmail.com>
2023-07-24 00:24:15 +0300

    create fancy design, bind callback api model to html

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,12 @@
-fetch("https://2k2agenv28.execute-api.eu-north-1.amazonaws.com/items").then(function (response) {
-    console.log(response.json().then(function (data) {
-        console.log(data);
+(function callTodayAiSuggestion() {
+    fetch("https://2k2agenv28.execute-api.eu-north-1.amazonaws.com/items").then(function (response) {
+console.log(response.json().then(function (fetchedData) {
+    console.log(fetchedData);
+        const data = fetchedData[0];
+        var title = document.getElementById("title");
+        var description = document.getElementById("description");
+        title.innerHTML = data.aiName;
+        description.innerHTML = data.description;
     }));
 })
+})();
