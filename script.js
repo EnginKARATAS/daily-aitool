3ed680e8914120373834821f0f959056b5825861
Engin Karataş <enginkaratas99@gmail.com>
2023-07-23 20:32:59 +0300

    popup js created and description changed

---
+++ script.js
@@ -0,0 +1,6 @@
+function    loadDailyAiSuggestion(){
+    var dailyAiSuggestion = document.getElementById("dailyAiSuggestion");
+    dailyAiSuggestion.innerHTML = "Hello World!";
+}
+
+addEventListener("load", loadDailyAiSuggestion);
