ad7bb99f570e743d2f9c35d154a7907eb071a459
Engin Karataş <enginkaratas99@gmail.com>
2023-08-06 22:48:58 +0300

    format popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,16 @@
 (function callTodayAiSuggestion() {
-    fetch("https://2k2agenv28.execute-api.eu-north-1.amazonaws.com/items").then(function (response) {
-console.log(response.json().then(function (fetchedData) {
-    console.log(fetchedData);
-        const data = fetchedData[0];
-        var title = document.getElementById("title");
-        var description = document.getElementById("description");
-        title.innerHTML = data.aiName;
-        description.innerHTML = data.description;
-    }));
-})
+  fetch("https://2k2agenv28.execute-api.eu-north-1.amazonaws.com/items").then(
+    function (response) {
+      console.log(
+        response.json().then(function (fetchedData) {
+          console.log(fetchedData);
+          const data = fetchedData[0];
+          var title = document.getElementById("title");
+          var description = document.getElementById("description");
+          title.innerHTML = data.aiName;
+          description.innerHTML = data.description;
+        })
+      );
+    }
+  );
 })();
